f00632944b5a1987e0ecf76f0823e40da5333b9e
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-04-08 12:52:34 +0530

    feat: add navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<nav class="container navbar mx-auto bg-base-100">
+	<!-- TODO: Placeholder icon -->
+	<a href="/" class="navbar-start flex items-center gap-2 text-xl">
+		<div class="avatar">
+			<div class="w-8 rounded">
+				<img src="/favicon.png" alt="logo" />
+			</div>
+		</div>
+		<span class="font-heading font-semibold"><span class="text-primary">Chat</span>Crafters</span>
+	</a>
+	<div class="navbar-end flex items-center gap-4">
+		<a href="/auth?mode=login" class="hover:brightness-110">Log in</a>
+		<a
+			href="/auth?mode=signup"
+			class="btn btn-primary no-animation btn-sm border border-white font-semibold"
+		>
+			Sign up
+		</a>
+	</div>
+</nav>
