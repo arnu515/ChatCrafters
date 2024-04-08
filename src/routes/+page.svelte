6709e82e189dbfd7cf8e5b461790284ae0e4f505
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-04-08 12:55:43 +0530

    feat: add basic landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<!-- TODO: MAKE BETTER -->
+
+<h1 class="font-heading mt-8 text-center text-5xl">
+	<span class="text-primary">Chat</span>Crafters
+</h1>
+<p class="mt-8 text-center text-3xl">Craft your conversations, meet virtual personas, connect!</p>
+<div class="mt-4 flex items-center justify-center gap-4">
+	<a href="/auth?mode=signup" class="btn btn-primary text-lg">Sign up</a>
+	<a href="/auth?mode=login" class="btn btn-outline btn-primary text-lg">Log in</a>
+</div>
+
