38d72d73b2dba1bf0d40ed41c6629aeeb1a9d770
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-04-10 22:42:21 +0530

    feat: add personas table

---
+++ sql/0002_add-personas-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE personas (
+	id text primary key,
+	name text not null,
+	summary text not null,
+	model text CHECK (model in ('@cf/tinyllama/tinyllama-1.1b-chat-v1.0', '@cf/meta/llama-2-7b-chat-int8', '@hf/mistralai/mistral-7b-instruct-v0.2')) not null,
+	prompt text not null,
+	attire text not null,
+	userId text not null REFERENCES users(id)
+);
+
--- src/lib/dbtypes.ts
+++ src/lib/dbtypes.ts
@@ -11,3 +11,13 @@ export interface User {
 export type SafeUser = Omit<Omit<User, "password">, "is_banned">
 export type SafeUserWithoutEmail = Omit<SafeUser, "email">
 
+export interface Persona {
+	id: string
+	name: string
+	summary: string
+	model: '@cf/tinyllama/tinyllama-1.1b-chat-v1.0' | '@cf/meta/llama-2-7b-chat-int8' | '@hf/mistralai/mistral-7b-instruct-v0.2'
+	prompt: string
+	attire: string
+	userId: string
+}
+
