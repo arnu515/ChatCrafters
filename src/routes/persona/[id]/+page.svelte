5af6c3dbed2aaf5d0fba3c92658d3c59b535dc3d
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-04-11 12:14:12 +0530

    feat: ability to share persona

--- src/routes/persona/[id]/+page.svelte
+++ src/routes/persona/[id]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Separator, Dialog } from 'bits-ui'
 	import { env } from '$env/dynamic/public'
-	import { SendIcon, TrashIcon, XIcon } from 'lucide-svelte'
+	import { SendIcon, TrashIcon, XIcon, CopyIcon } from 'lucide-svelte'
 	import dayjs from 'dayjs'
 	import rt from 'dayjs/plugin/relativeTime.js'
 	import { fade, fly } from 'svelte/transition'
@@ -19,6 +19,26 @@
 
 	export let data
 	export let form
+
+	function copyShareLink() {
+		const input = document.getElementById('share-link') as HTMLInputElement
+		if (!input) return
+
+		if ('clipboard' in navigator && 'writeText' in navigator.clipboard) {
+			navigator.clipboard.writeText(input.value)
+		} else {
+			input.select()
+			document.execCommand('copy')
+		}
+	}
+	function openShareSheet() {
+		const input = document.getElementById('share-link') as HTMLInputElement
+		if (!input) return
+
+		if ('share' in navigator) {
+			navigator.share({ url: input.value })
+		}
+	}
 </script>
 
 <main
@@ -42,7 +62,63 @@
 					{#if data.persona.userId === data.user?.id}
 						<button class="btn btn-neutral btn-sm">Edit Persona</button>
 					{/if}
-					<button class="btn btn-primary btn-sm">Share Persona</button>
+					<Dialog.Root>
+						<Dialog.Trigger class="btn btn-primary btn-sm">Share Persona</Dialog.Trigger>
+						<Dialog.Portal>
+							<Dialog.Overlay
+								transition={fade}
+								transitionConfig={{ duration: 200 }}
+								class="fixed inset-0 z-10 bg-black/70"
+							/>
+
+							<Dialog.Content
+								transition={fly}
+								class="fixed left-[50%] top-[50%] z-10 w-full max-w-md translate-x-[-50%] translate-y-[-50%] rounded-lg border-2 border-primary/80 bg-base-300 p-4 shadow-md outline-none"
+							>
+								<Dialog.Title
+									class="flex items-center justify-center text-center text-lg font-semibold text-black dark:text-white"
+									>Share this Persona
+									<Dialog.Close class="ml-auto rounded-full p-2 active:scale-95">
+										<XIcon class="h-5 w-5" />
+									</Dialog.Close>
+								</Dialog.Title>
+								<p class="my-4 flex items-center font-mono">
+									Share <span class="font-heading mx-3 inline-flex items-center gap-2">
+										<img
+											src={`${env.PUBLIC_S3_CDN_URL}/persona_avatars/${data.persona.id}.png`}
+											class="h-6 w-6 rounded-full"
+											alt="Persona's avatar"
+										/>
+										{data.persona.name}
+									</span> with everyone!
+								</p>
+								<label for="share-link">
+									<div class="label">
+										<span class="label-text">Enter report</span>
+									</div>
+									<div class="flex items-center gap-2">
+										<input
+											id="share-link"
+											readonly
+											class="input input-bordered w-full font-mono text-base placeholder:text-gray-300 dark:placeholder:text-gray-600"
+											value={new URL(window.location.href).origin + '/persona/' + data.persona.id}
+										/>
+										<button class="btn btn-square btn-neutral" on:click={copyShareLink}
+											><CopyIcon class="h-5 w-5" /></button
+										>
+									</div>
+								</label>
+								<div class="mt-4 flex items-center gap-2">
+									<Dialog.Close type="button" class="btn btn-outline btn-neutral mr-auto text-lg"
+										>Close</Dialog.Close
+									>
+									<button class="btn btn-primary text-lg" on:click={openShareSheet}
+										>Open share sheet</button
+									>
+								</div>
+							</Dialog.Content>
+						</Dialog.Portal>
+					</Dialog.Root>
 				</div>
 			</div>
 		</div>
