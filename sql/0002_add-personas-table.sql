2051ab77f92855b49f28f3f8910bfefd3e8e381f
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-04-11 16:52:53 +0530

    fix: use `CASCADE` for deletes and updates on fkeys

--- sql/0002_add-personas-table.sql
+++ sql/0002_add-personas-table.sql
@@ -5,6 +5,6 @@ CREATE TABLE personas (
 	model text CHECK (model in ('@cf/tinyllama/tinyllama-1.1b-chat-v1.0', '@cf/meta/llama-2-7b-chat-int8', '@hf/mistralai/mistral-7b-instruct-v0.2')) not null,
 	prompt text not null,
 	attire text not null,
-	userId text not null REFERENCES users(id)
+	userId text not null REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
