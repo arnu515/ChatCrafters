cb5bd6bf3c74aa8749e38e180115aaed0013659b
arnu515 <52203828+arnu515@users.noreply.github.com>
2024-04-13 11:19:19 +0530

    fix: add cascade

--- sql/0005_add-voice_activity-table.sql
+++ sql/0005_add-voice_activity-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE voice_activity (
-	userId text primary key references users(id),
+	userId text primary key references users(id) on delete cascade on update cascade,
 	amount int not null default 0,  -- in seconds
 	d text not null default CURRENT_DATE  -- iso 8601 date string (YYYY-MM-DD)
 );
